4d585f7c4ef79a05e8142330a799da86009cc360
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-08-04 11:35:35 -0700

    Add js files, install npm, node and bower

---
+++ js/doctor.js
@@ -0,0 +1,18 @@
+var apiKey = require('./../.env').apiKey;
+
+function Search(){
+}
+
+Search.prototype.getDoctors = function (medicalIssue, displayDoctors) {
+  this.medicalIssue = medicalIssue;
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  .then(function(result) {
+    this.searchResults=result
+    displayDoctors(result);
+  })
+  .fail(function(error){
+    console.log("fail");
+  });
+};
+
+exports.doctorSearch = Search;
